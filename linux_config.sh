a99b111ffb2be52493a01a4ca58caa151da24b06
panwenhang <panwenhang92@gmail.com>
2014-10-28 12:28:32 +0800

    deleted:    .vim/.vim 	modified:   linux_config.sh

--- linux_config.sh
+++ linux_config.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
+set -x
+
 shell_dir=$(cd `dirname $0`; pwd);
 
 if [ -f $shell_dir/.bashrc.pwh ]; then
-	ln -sf $shell_dir/.bashrc.pwh ~/.bashrc.pwh;
+	ln -sf $shell_dir/.bashrc.pwh ~/.bashrc.pwh
 fi
 
 if [ -d $shell_dir/.vim ]; then
-	ln -sf $shell_dir/.vim ~/.vim;
+	ln -sf $shell_dir/.vim ~
 fi
 
 if [ -f $shell_dir/.vimrc ]; then
-	ln -sf $shell_dir/.vimrc ~/.vimrc;
+	ln -sf $shell_dir/.vimrc ~/.vimrc
 fi
 
 if [ `whoami` == "root" ]; then
 	if [ -f $shell_dir/hosts ] && [ -n "`diff /etc/hosts $shell_dir/hosts`" ]; then
-		ln -sf $shell_dir/hosts /etc/hosts;
+		ln -sf $shell_dir/hosts /etc/hosts
 	fi
 	
 	if [ -n "`grep -i kali /etc/issue`" ] && [ -f $shell_dir/sources.list.kali ] && [ -n "`diff /etc/apt/sources.list $shell_dir/sources.list.kali`" ]; then
-		ln -sf $shell_dir/sources.list.kali /etc/apt/sources.list;
+		ln -sf $shell_dir/sources.list.kali /etc/apt/sources.list
 	fi
 fi
 
